4c175aa4b8c30099cdf0528b437bd735f910faba
Anthony Bordg <bordg.anthony@gmail.com>
2024-10-09 12:46:43 +0200

    delete the import of Basic

--- ToposTheory.lean
+++ ToposTheory.lean
@@ -1,3 +1,2 @@
 -- This module serves as the root of the `ToposTheory` library.
 -- Import modules here that should be built as part of the library.
-import ToposTheory.Basic
--- ToposTheory/GrothendieckTopos.lean
+++ ToposTheory/GrothendieckTopos.lean
@@ -5,6 +5,8 @@ Authors: Anthony Bordg
 -/
 import Mathlib.CategoryTheory.Sites.Sheaf
 import Mathlib.CategoryTheory.Equivalence
+import Mathlib.CategoryTheory.Sites.Sheafification
+import Mathlib.CategoryTheory.Sites.Equivalence
 
 /-!
 # Grothendieck Toposes
@@ -28,33 +30,47 @@ namespace GrothendieckTopos
 
 /-! ### Grothendieck toposes -/
 
-universe u v w
+universe w
 
 variable {C : Type w} [SmallCategory C] (J : GrothendieckTopology C)
-variable {E : Type u} [Category.{v} E] (l : C ⥤ E)
 
 namespace InducedPresentation
 
-theorem isSheaf_of_induced_presentation (X : E) : Presheaf.IsSheaf J (l.op ⋙ yoneda.obj X) := by admit
+universe u v
 
-def inducedPresentation : E ⥤ Sheaf J (Type v) where
-  obj := fun X => ⟨l.op ⋙ yoneda.obj X, isSheaf_of_induced_presentation J l X⟩
-  map := fun f => ⟨whiskerLeft l.op (yoneda.map f)⟩
+variable {E : Type u} [Category.{v} E] (l : C ⥤ E)
+
+--theorem isSheaf_of_induced_presentation (X : E) : Presheaf.IsSheaf J (l.op ⋙ yoneda.obj X) := by admit
+
+def inducedPresentation : E ⥤ (Cᵒᵖ ⥤ (Type v)) where
+  --obj := fun X => ⟨l.op ⋙ yoneda.obj X, isSheaf_of_induced_presentation J l X⟩
+  --map := fun f => ⟨whiskerLeft l.op (yoneda.map f)⟩
+  obj := fun X => l.op ⋙ yoneda.obj X
+  map := fun f => whiskerLeft l.op (yoneda.map f)
 
 open Sieve Presieve
 
-theorem covering_family_iff {X : C} (P : Presieve X) (h : (inducedPresentation J l).IsEquivalence) :
+/-theorem covering_family_iff {X : C} (P : Presieve X) (h : (inducedPresentation J l).IsEquivalence) :
   generate P ∈ J X ↔ JointlyEpicFam (functorPushforward l P) := by admit
+-/
 
 end InducedPresentation
 
 open InducedPresentation
 
-class Topos (ℰ : Type*) [Category ℰ] : Prop where
+/-class Topos (ℰ : Type*) [Category ℰ] : Prop where
   presentation : ∃ (C : Type w) (_ : SmallCategory C) (J : GrothendieckTopology C) (l : C ⥤ ℰ),
     (inducedPresentation J l).IsEquivalence
+-/
+class Topos (ℰ : Type*) [Category ℰ] : Prop where
+  presentation : ∃ (C : Type w) (_ : SmallCategory C) (J : GrothendieckTopology C) (l : C ⥤ ℰ)
+    (F : ℰ ≌ Sheaf J (Type w)), F.functor ⋙ sheafToPresheaf J (Type w) = inducedPresentation l
 
 instance : Topos.{w} (Sheaf J (Type w)) where
-  presentation := by admit
+  presentation := by
+    use C, inferInstance, J
+    have : HasWeakSheafify J (Type w) := by
+      exact instHasWeakSheafifyOfHasSheafify J (Type w)
+    use yoneda ⋙ presheafToSheaf J (Type w)
 
 end GrothendieckTopos
